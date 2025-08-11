fe183a324359cb9ad06ff6220ae063dda995ea99
codeislive63 <codeislive63@gmail.com>
2025-08-11 11:17:23 +0300

    Initial Commit

---
+++ src/codeislive63/delegates/generic/Action.java
@@ -0,0 +1,17 @@
+package codeislive63.delegates.generic;
+
+/**
+ * Представляет действие, которое принимает один аргумент и не возвращает результата.
+ *
+ * @param <T> Тип аргумента действия.
+ */
+@FunctionalInterface
+public interface Action<T> {
+
+    /**
+     * Выполняет действие с заданным аргументом.
+     *
+     * @param arg Аргумент действия.
+     */
+    void invoke(T arg);
+}
